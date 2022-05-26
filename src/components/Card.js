83766d27c312a3c383919f61da4e9c587a1c26e4
diego-008 <diegoacetun00345@gmail.com>
2022-05-26 17:00:27 -0600

    Se muestran las peliculas

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
 export default function Card(props) {
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img src={props.image} className="card-img-top" alt="..." />
-      <div className="card-body">
-        <p className="card-text">
-          {props.title}
-        </p>
+    <div className="col-md-4 mb-3 d-flex justify-content-center">
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={props.image} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <p className="card-text">
+            {props.title}
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -3,79 +3,71 @@ import "./CSS/Movies.css";
 import Card from "../components/Card";
 export function Movies() {
   const apiKey = process.env.REACT_APP_KEY_OMDB;
-  const [search, setSearch] = useState("batman");
+  const [searchValue, setSearchValue] = useState("pan");
   const [movies, setMovies] = useState([]);
-  const searchValue = useRef("");
+  const search = useRef();
+  const respuesta = useRef();
   useEffect(() => {
     async function fetchData() {
-      const URL = `https://www.omdbapi.com/?apikey=${apiKey}&s=${search}`;
+      const URL = `https://www.omdbapi.com/?apikey=${apiKey}&s=${searchValue}`;
       const response = await fetch(URL);
+      // console.log(response);
       const data = await response.json();
-      setMovies(data.Search);
       console.log(data);
+      // respuesta = data.Response;
+      respuesta.current = data.Response;
+      console.log(respuesta, "aa");
+      setMovies(data.Search);
+      // console.log(data);
     }
     fetchData();
     console.log("Movies.js");
-  }, [search]);
+  }, [searchValue, apiKey]);
 
   function onSubmit(e) {
     e.preventDefault();
-    console.log(searchValue.current.value); 
-    setSearch(searchValue.current.value);
-    
+    // console.log(searchValue.current.value);
+    // console.log(search.current.value, 'aaa');
+    setSearchValue(search.current.value);
+    search.current.value = "";
   }
-  const showMovies = movies.map((movie, index) => {
-      return (
-        // <div className="card" key={index}>
-        //   <img src={movie.Poster} className="card-img-top" alt="..." />
-        //   <div className="card-body">
-        //     <p className="card-text">
-        //       {movie.Title}
-        //     </p>
-        //   </div>
-        // </div>
-        <Card title={movie.Title} image={movie.Poster} key={movie.imdbID} />
-      );
-    })
-    let a = 0;
+  function res() {
+    // console.log(respuesta);
+    if (respuesta.current === "False") {
+      // console.log('false');
+      return <h1 className="text-uppercase text-center text-light">No matches found</h1>;
+    }
+    console.log("true");
+    return (
+      <div className="row border justify-content-center align-items-center">
+        {movies.map((movie) => {
+          return (
+            <Card title={movie.Title} image={movie.Poster} key={movie.imdbID} />
+          );
+        })}
+      </div>
+    );
+  }
   return (
     <div className="container">
       <h1 className="text-light text-center mt-3">MOVIES APP</h1>
       <form onSubmit={onSubmit}>
-        <div className="col-md-4 offset-md-4">
+        <div className="col-md-4 offset-md-4 off">
           <div className="input-group mb-3">
             <input
               type="text"
               className="form-control"
               placeholder="search"
-              ref={searchValue}
+              ref={search}
               // defaultValue={search}
-              
             />
-            <button
-              className="btn btn-info"
-              type="submit"
-              id="button-addon2"
-            >
+            <button className="btn btn-info" type="submit" id="button-addon2">
               Search
             </button>
           </div>
         </div>
-      </form>      
-      {
-        
-        movies.map((movie, index) => {
-        if(a===3){
-          // console.log(a);
-          a = 0;
-        }
-        console.log(a);
-        a = a+1;
-          return (
-            <Card title={movie.Title} image={movie.Poster} key={movie.imdbID} />
-          );
-        })}
+      </form>
+      {res()}
     </div>
   );
-
 }
