bf1eeb9efaf77bcb1b468f99e1a1977b56d0f38f
diego-008 <diegoacetun00345@gmail.com>
2022-05-30 12:31:13 -0600

    Trabajando con state

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -10,38 +10,54 @@ export function Movies() {
   const [page, setPage] = useState(1);
   const search = useRef();
   // TODO: usar state para la respuesta
-  const respuesta = useRef();
+  const [response, setResponse] = useState("False");
+  const [stateMovies, setStateMovies] = useState({
+    searchValue: "pan",
+    bienvenida: true,
+    type: "",
+    page: 1,
+  });
+  console.log("respuesta");
   useEffect(() => {
-    if (bienvenida) {
+    if (stateMovies.bienvenida) {
       return;
     }
     // console.log("executed");
     async function fetchData() {
       try {
-        console.log(page);
-        const URL = `https://www.omdbapi.com/?apikey=${apiKey}&s=${searchValue}&type=${type}&page=${page}`;
+        // console.log(page);
+        const URL = `https://www.omdbapi.com/?apikey=${apiKey}&s=${stateMovies.searchValue}&type=${stateMovies.type}&page=${stateMovies.page}`;
+        console.log(URL);
         const response = await fetch(URL);
         const data = await response.json();
-        // console.log(data.Search);
-        respuesta.current = data.Response;
+        console.log(data.Response);
+        setResponse(data.Response);
+
         setMovies(data.Search);
       } catch (error) {
         console.log(error);
       }
     }
     fetchData();
-  }, [searchValue, apiKey, bienvenida, type, page]);
+  }, [stateMovies, apiKey]);
 
   function onSubmit(e) {
     e.preventDefault();
     // console.log(searchValue.current.value);
     // console.log(search.current.value, 'aaa');
     setSearchValue(search.current.value);
-    search.current.value = "";
     const type = document.querySelector('input[name="type"]:checked').value;
     setType(type);
     setBienvenida(false);
     setPage(1);
+    setStateMovies({
+      ...stateMovies,
+      searchValue: search.current.value,
+      bienvenida: false,
+      type: type,
+      page: 1,
+    });
+    search.current.value = "";
     // console.log(type);
     // console.log(document.querySelector('input[name=type]:checked').value);
     // const yy = document.querySelector("input[name=type]:checked").value;
@@ -50,17 +66,19 @@ export function Movies() {
   }
 
   function next() {
-    if (respuesta.current === "True") {
+    if (response === "True") {
       setPage(page + 1);
+      setStateMovies({ ...stateMovies, page: page + 1 });
     }
   }
   function before() {
     if (page > 1) {
       setPage(page - 1);
+      setStateMovies({ ...stateMovies, page: page - 1 });
     }
   }
   function res() {
-    if (bienvenida) {
+    if (stateMovies.bienvenida) {
       return (
         <div className="bienvenida">
           <h1 className="text-light text-uppercase text-center">
@@ -70,7 +88,7 @@ export function Movies() {
       );
     }
     // const response = await respuesta.current;
-    if (respuesta.current === "False") {
+    if (response === "False") {
       return (
         <h1 className="text-uppercase text-center text-light">
           sorry No results found
